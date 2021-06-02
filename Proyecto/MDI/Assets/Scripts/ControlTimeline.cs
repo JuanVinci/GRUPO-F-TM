dc1c8ee06ca7ab3cf1bcdff98a92bd12d3108e19
Nicolas-Soliz-UTN <gabriel.soliz@alumnos.frm.utn.edu.ar>
2021-06-02 10:25:32 -0300

    Animaciones y Scripts

---
+++ Proyecto/MDI/Assets/Scripts/ControlTimeline.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+
+public class ControlTimeline : MonoBehaviour
+{
+    private PlayableDirector timeline;
+
+    private PlayableDirector Timeline 
+    { 
+        get
+        {
+            if (null == timeline)
+                timeline = GetComponent<PlayableDirector>();
+
+            return timeline;
+        } 
+    }
+
+    public void Play()
+    {
+        Timeline.playableGraph.GetRootPlayable(0).SetSpeed(1);
+    }
+
+    public void Pause()
+    {
+        Timeline.playableGraph.GetRootPlayable(0).SetSpeed(0);
+    }
+}
